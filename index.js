a218142705d2983931039a505eb5d32e468d165c
amongjiwo <dahlanakhmad@gmail.com>
2022-05-19 15:05:46 +0700

    install ejs

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ const PORT = 8080;
 
 const app = express();
 
+app.use(express.urlencoded({ extended: false }));
+app.set("view engine", "ejs");
+
 app.listen(PORT, () => {
     console.log(`Listening on http://localhost:${PORT}`);
 });
